81c69b110a086aab15b9e4a7780ebcc588b4c562
Tim Cooijmans <t.cooijmans@student.maastrichtuniversity.nl>
2013-09-21 16:33:49 +0200

    convert radians to degrees

--- draw/drawRobots.m
+++ draw/drawRobots.m
@@ -7,7 +7,7 @@ for i = 1: length(robots.direction)
     y = robots.position(i,2);
     file = strcat('images/',colors{i},'.png');
     image = imread(file);
-    rotated = imrotate(image,direction_get_angle(robots.direction(i, :)));
+    rotated = imrotate(image,radtodeg(direction_get_angle(robots.direction(i, :))));
     imagesc([x+resize,x+1-resize],[y+resize,y+1-resize],rotated);
 end
   
