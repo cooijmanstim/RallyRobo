d463a16d9d2d5d3102a1f7193b8fa42b37aee2f9
Tim Cooijmans <t.cooijmans@student.maastrichtuniversity.nl>
2013-09-21 16:40:14 +0200

    clear environment, use random board

--- main.m
+++ main.m
@@ -1,8 +1,10 @@
+clear; clc;
+
 init();
 close();
 global BoardFigure;
-%game = game_create_random(12, 12, 2, 4);
-game = game_create_example();
+game = game_create_random(12, 12, 2, 4);
+%game = game_create_example();
 
 
 initBoardFigure(game);
