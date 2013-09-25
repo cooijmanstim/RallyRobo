c27768b23e8603c32178575c6ee1d94a666e0e3d
Tim Cooijmans <t.cooijmans@student.maastrichtuniversity.nl>
2013-09-25 17:11:44 +0200

    automated test for robot_can_leave

---
+++ test/test_robot_can_leave.m
@@ -0,0 +1,17 @@
+function [success] = test_robot_can_leave()
+global RR;
+
+game = game_create_example();
+
+% the red robot
+irobot = 3;
+x = game.state.robots.position(irobot, :);
+% should be able to leave in any direction
+dxs = RR.directions.asrows;
+
+for i = 1:RR.ndirections;
+    if ~robot_can_leave(game, irobot, x, dxs(i, :))
+        success = false;
+        return;
+    end
+end
