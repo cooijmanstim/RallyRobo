5f41de32815d84a0d7b9c39cba3105d9b8bce80b
Tim Cooijmans <t.cooijmans@student.maastrichtuniversity.nl>
2013-09-28 13:53:33 +0200

    condense, use abstractions

--- board_create_example.m
+++ board_create_example.m
@@ -1,62 +1,19 @@
 function [board] = board_create_example()
-% board(i, j, k) is one if the kth feature (see tfi) is present in the tile
-% at (i, j)
 global RR;
 board = zeros(12, 12, RR.nfeatures);
 
-% repair statons
-board(2,1,2) = 1;
-board(2,5,2) = 1;
-board(5,3,2) = 1;
-board(9,3,2) = 1;
-board(5,8,2) = 1;
-board(1,10,2) = 1;
-
-% conveyor belt
-board(3,1,8) = 1;
-board(3,2,8) = 1;
-board(3,3,8) = 1;
-board(3,4,7) = 1;
-board(3,4,11) = 1;
-board(4,4,7) = 1;
-board(5,4,7) = 1;
-board(6,4,7) = 1;
-board(7,4,8) = 1;
-board(7,4,12) = 1;
-board(7,5,8) = 1;
-board(7,6,7) = 1;
-board(7,6,11) = 1;
-board(8,6,7) = 1;
-board(9,6,7) = 1;
-board(10,6,7) = 1;
-board(11,6,7) = 1;
-board(12,6,7) = 1;
-
-% pits
-
-board(8,5,1) = 1;
-board(9,5,1) = 1;
-board(3,7,1) = 1;
-board(6,10,1) = 1;
-
-% walls
-
-% east
-board(4,5,3) = 1;
-
-%north
-board(10,2,4) = 1;
-board(4,5,4) = 1;
-board(7,7,4) = 1;
-board(3,10,4) = 1;
-
-%west
-board(4,1,5) = 1;
-board(10,2,5) = 1;
-board(7,3,5) = 1;
-board(1,7,5) = 1;
-board(11,9,5) = 1;
-board(5,12,5) = 1;
-
-%south
-board(6,1,6) = 1;
+for o = {RR.features.repair         [2 1; 2 5; 5 3; 9 3; 5 8; 1 10]
+         RR.features.conveyor_north [3 1; 3 2; 3 3; 7 4; 7 5]
+         RR.features.conveyor_east  [3 4; 4 4; 5 4; 6 4; 7 6; 8 6; 9 6; 10 6; 11 6; 12 6]
+         RR.features.conveyor_turning_clockwise [7 6; 3 4]
+         RR.features.conveyor_turning_counterclockwise [7 4]
+         RR.features.pit            [8 5; 9 5; 3 7; 6 10]
+         RR.features.wall_east      [4 5]
+         RR.features.wall_north     [10 2; 4 5; 7 7; 3 10]
+         RR.features.wall_west      [4 1; 10 2; 7 3; 1 7; 11 9; 5 12]
+         RR.features.wall_south     [6 1]}'
+    feature = o{1}; xys = o{2};
+    for i = 1:size(xys, 1)
+        board(xys(i, 1), xys(i, 2), feature) = 1;
+    end
+end
