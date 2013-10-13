f161b096b95850e4845ec25b183ba30fe77d6739
Tim Cooijmans <t.cooijmans@student.maastrichtuniversity.nl>
2013-10-13 09:47:42 +0200

    fix and speed up blotching

--- image_blotch_slightly.m
+++ image_blotch_slightly.m
@@ -3,10 +3,18 @@ if isinteger(x)
     x = double(x)/255;
 end
 center = arrayfun(@(n) randi(n, 1), size(x));
-[xs ys zs] = ndgrid(1:size(x, 1), 1:size(x, 2), 1:size(x, 3));
-S = diag(1./size(x)); % why no color imbalance?
-f = @(x) x*S*x';
-blotch = arrayfun(@(x, y, z) f([x y z] - center), xs, ys, zs);
+S = diag(1./(size(x).^2));
+
+blotch = zeros(size(x));
+for i = 1:size(x, 1)
+    for j = 1:size(x, 2)
+        for k = 1:size(x, 3)
+            v = [i j k] - center;
+            blotch(i, j, k) = v*S*v';
+        end
+    end
+end
+
 blotch = blotch/max(blotch(:));
 
 sign = randi(1, 1)*2 - 1;
